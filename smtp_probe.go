06f733290eb1e13f3c9f8df089471db67856b120
Steve Kemp <steve@steve.org.uk>
2018-04-12 13:50:08 +0300

    Added SMTP protocol-tester (fake).

---
+++ smtp_probe.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"net"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//
+// Our structure.
+//
+// We store state in the `input` field.
+//
+type SMTPTest struct {
+	input string
+}
+
+//
+// Run the test against the specified target.
+//
+func (s *SMTPTest) runTest(target string) error {
+	var err error
+
+	//
+	// The default port to connect to.
+	//
+	port := 25
+
+	//
+	// If the user specified a different port update it.
+	//
+	re := regexp.MustCompile("on\\s+port\\s+([0-9]+)")
+	out := re.FindStringSubmatch(s.input)
+	if len(out) == 2 {
+		port, err = strconv.Atoi(out[1])
+		if err != nil {
+			return err
+		}
+	}
+
+	//
+	// Set an explicit timeout
+	//
+	d := net.Dialer{Timeout: time.Second * 10}
+
+	//
+	// Make the TCP connection.
+	//
+	conn, err := d.Dial("tcp", fmt.Sprintf("%s:%d", target, port))
+	if err != nil {
+		return err
+	}
+
+	//
+	// Read the banner.
+	//
+	banner, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		return err
+	}
+	conn.Close()
+
+	if !strings.Contains(banner, "SMTP") {
+		return errors.New("Banner doesn't look like an SMTP server")
+	}
+
+	return nil
+}
+
+//
+// Store the complete line from the parser in our private
+// field; this could be used if there are protocol-specific options
+// to be understood.
+//
+func (s *SMTPTest) setLine(input string) {
+	s.input = input
+}
+
+//
+// Register our protocol-tester.
+//
+func init() {
+	Register("smtp", func() ProtocolTest {
+		return &SMTPTest{}
+	})
+}
