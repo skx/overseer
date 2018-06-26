201206f139da3482c74c6a68016fd775753cd877
Steve Kemp <steve@steve.org.uk>
2018-06-26 10:51:13 +0300

    Updated our SMTP-probe to perform SMTP authentication.
    This commit updates the SMTP-probe to support optional
    SMTP-authentication.  If a username/password are supplied
    they will be tested against the remote host.
    
    NOTE: Authentication requires the use of STARTTLS, to avoid
    submitting credentials in plaintext, so there is an optional
    `with tls insecure` support to disable any warnings about
    unknown CAs, or hostname mismatches.
    
    This closes #33.

--- protocols/smtp_probe.go
+++ protocols/smtp_probe.go
@@ -1,20 +1,34 @@
 // SMTP Tester
 //
-// The SMTP tester connects to a remote host and ensures that a response
-// is received that looks like an SMTP-server banner.
+// The SMTP tester checks on the status of a remote SMTP-server.
 //
 // This test is invoked via input like so:
 //
 //    host.example.com must run smtp [with port 25]
 //
+// By default a connection will be attempted and nothing else.  A more
+// complete test would be to specify a username & password and test that
+// authentication succeeds.
+//
+// Note that performing an authentication-request requires the use of
+// `STARTTLS`.  If the TLS certificate is self-signed or otherwise
+// non-trusted you'll need to disable the validity checking by appending
+// `with tls insecure`.
+//
+// A complete example, testing a login, will look like this:
+//
+//    host.example.com must run smtp [with port 587] with username 'steve@example.com' with password 'secret'  [with tls insecure]
+//
+//
 
 package protocols
 
 import (
-	"bufio"
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"net"
+	"net/smtp"
 	"strconv"
 	"strings"
 
@@ -30,7 +44,10 @@ type SMTPTest struct {
 // their values.
 func (s *SMTPTest) Arguments() map[string]string {
 	known := map[string]string{
-		"port": "^[0-9]+$",
+		"port":     "^[0-9]+$",
+		"username": ".*",
+		"password": ".*",
+		"tls":      "insecure",
 	}
 	return known
 }
@@ -40,12 +57,24 @@ func (s *SMTPTest) Example() string {
 	str := `
 SMTP Tester
 -----------
- The SMTP tester connects to a remote host and ensures that a response
- is received that looks like an SMTP-server banner.
+ The SMTP tester checks on the status of a remote SMTP-server.
 
  This test is invoked via input like so:
 
-    host.example.com must run smtp
+    host.example.com must run smtp [with port 25]
+
+ By default a connection will be attempted and nothing else.  A more
+ complete test would be to specify a username & password and test that
+ authentication succeeds.
+
+ Note that performing an authentication-request requires the use of
+ STARTTLS.  If the TLS certificate is self-signed or otherwise
+ non-trusted you'll need to disable the validity checking by appending
+ 'with tls insecure'.
+
+ A complete example, testing a login, will look like this:
+
+    host.example.com must run smtp [with port 587] with username 'steve@example.com' with password 's3cr3t'  [with tls insecure]
 `
 	return str
 }
@@ -98,19 +127,67 @@ func (s *SMTPTest) RunTest(tst test.Test, target string, opts test.TestOptions) 
 		return err
 	}
 
-	//
-	// Read the banner.
-	//
-	banner, err := bufio.NewReader(conn).ReadString('\n')
+	// The default TLS configuration verifies the certificate
+	// matches the hostname of our target.
+	tlsconfig := &tls.Config{
+		ServerName: tst.Target,
+	}
+
+	// However if the user is being insecure then we'll validate
+	// nothing - allowing self-signed certificates, and hostname
+	// mismatches.
+	if tst.Arguments["tls"] == "insecure" {
+		tlsconfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	}
+
+	// Create the SMTP-client
+	client, err := smtp.NewClient(conn, tst.Target)
 	if err != nil {
 		return err
 	}
-	conn.Close()
 
-	if !strings.Contains(banner, "SMTP") {
-		return errors.New("Banner doesn't look like an SMTP server")
+	defer client.Close()
+
+	if err = client.Hello(tst.Target); err != nil {
+		return err
 	}
 
+	//
+	// If we have a username & password then we have to
+	// try them - but this will require TLS so we'll start
+	// that first.
+	//
+	if tst.Arguments["username"] != "" &&
+		tst.Arguments["password"] != "" {
+
+		hasStartTLS, _ := client.Extension("STARTTLS")
+		if !hasStartTLS {
+			return errors.New("We cannot login without STARTTLS, and that was not advertised.")
+		}
+
+		if err = client.StartTLS(tlsconfig); err != nil {
+			return err
+		}
+
+		//
+		// In the future we might try more options
+		//
+		// CRAM MD5 is available in the net/smtp client at least.
+		//
+		auth := smtp.PlainAuth("", tst.Arguments["username"],
+			tst.Arguments["password"], tst.Target)
+
+		//
+		// If auth failed then report that.
+		//
+		if err = client.Auth(auth); err != nil {
+			return err
+		}
+	}
+
+	// All done
 	return nil
 }
 
