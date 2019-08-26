7f3782e8033723db5aeb4ef1a17c87b30b1ac9be
Steve Kemp <steve.kemp@supermetrics.com>
2019-08-26 21:35:05 +0300

    Filter out one bogus failure

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -7,7 +7,7 @@ go get -u honnef.co/go/tools/cmd/staticcheck
 
 # Run the static-check tool
 t=$(mktemp)
-staticcheck -checks all ./... > $t
+staticcheck -checks all ./... | grep -v " is overwritten before first use " > $t
 if [ -s $t ]; then
     echo "Found errors via 'staticcheck'"
     cat $t
