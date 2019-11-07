ee46a89433e12e0a4dbdda1d6dd66fed3c0ea86f
Steve Kemp <steve@steve.org.uk>
2019-11-07 20:34:31 +0200

    Allow testing set/list length.
    This closes #44.

--- protocols/redis_probe.go
+++ protocols/redis_probe.go
@@ -7,6 +7,19 @@
 //
 //    host.example.com must run redis [with port 6379] [with password 'password']
 //
+// If you wish you can test the size of a set/list, this is not quite as
+// good as it might be as our argument-parsing is a little too strict.
+//
+// To make sure the list `steve` has no more than `1000` entries we
+// would write:
+//
+//   localhost must run redis with list 'steve' with max_size '1000'
+//
+// Or the set `users`:
+//
+//   localhost must run redis with set 'members' with max_size '1000'
+//
+//
 
 package protocols
 
@@ -28,8 +41,11 @@ type REDISTest struct {
 // their values.
 func (s *REDISTest) Arguments() map[string]string {
 	known := map[string]string{
-		"port":     "^[0-9]+$",
+		"list":     ".*",
+		"set":      ".*",
+		"max_size": "^[0-9]+$",
 		"password": ".*",
+		"port":     "^[0-9]+$",
 	}
 	return known
 }
@@ -44,7 +60,19 @@ Redis Tester
 
  This test is invoked via input like so:
 
-    host.example.com must run redis
+    host.example.com must run redis [with password 'secret'] [with port '6379']
+
+ If you wish you can test the size of a set/list, this is not quite as
+ good as it might be as our argument-parsing is a little too strict.
+
+ To make sure the list 'steve' has no more than 1000 entries we
+ would write:
+
+   localhost must run redis with list 'steve' with max_size '1000'
+
+ Or the set 'users':
+
+   localhost must run redis with set 'members' with max_size '1000'
 `
 	return str
 }
@@ -82,6 +110,17 @@ func (s *REDISTest) RunTest(tst test.Test, target string, opts test.Options) err
 	}
 
 	//
+	// Maximum list-size, if specified
+	//
+	maxSize := 0
+	if tst.Arguments["max_size"] != "" {
+		maxSize, err = strconv.Atoi(tst.Arguments["max_size"])
+		if err != nil {
+			return err
+		}
+	}
+
+	//
 	// If the user specified a password use it.
 	//
 	password = tst.Arguments["password"]
@@ -108,7 +147,7 @@ func (s *REDISTest) RunTest(tst test.Test, target string, opts test.Options) err
 	})
 
 	//
-	// And run a ping
+	// Now test the connection by running a ping
 	//
 	// If the connection is refused, or the auth-details don't match
 	// then we'll see that here.
@@ -116,6 +155,54 @@ func (s *REDISTest) RunTest(tst test.Test, target string, opts test.Options) err
 	_, err = client.Ping().Result()
 	if err != nil {
 		return err
+	}
+
+	//
+	// If we have a `list` and a `max_size` then get the size of the
+	// specified set.
+	//
+	if tst.Arguments["list"] != "" && maxSize > 0 {
+
+		//
+		// Get the length of the list.
+		//
+		res := client.LLen(tst.Arguments["list"])
+		if res.Err() != nil {
+			return res.Err()
+		}
+
+		len := int(res.Val())
+
+		//
+		// Raise an alert if the size is exceeded.
+		//
+		if len >= maxSize {
+			return (fmt.Errorf("list %s has %d entries, more than the max size of %d", tst.Arguments["list"], len, maxSize))
+		}
+	}
+
+	//
+	// If we have a `set` and a `max_size` then get the size of the
+	// specified set.
+	//
+	if tst.Arguments["set"] != "" && maxSize > 0 {
+
+		//
+		// Get the count of set-members.
+		//
+		res := client.SCard(tst.Arguments["list"])
+		if res.Err() != nil {
+			return res.Err()
+		}
+
+		len := int(res.Val())
+
+		//
+		// Raise an alert if the size is exceeded.
+		//
+		if len >= maxSize {
+			return (fmt.Errorf("set %s has %d members, more than the max size of %d", tst.Arguments["list"], len, maxSize))
+		}
 	}
 
 	//
